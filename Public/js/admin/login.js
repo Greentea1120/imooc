d817ebf13b2c1c2dd8c60a22f641a5ed345ab20a
greentea <741024920@qq.com>
2017-03-08 16:12:38 +0800

    login

---
+++ Public/js/admin/login.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/3/8 0008.
+ */
+/*
+* 前端业务类
+* @author greentea
+*/
+
+var login = {
+    check : function () {
+        //获取登录页面中的用户名 密码
+        var username = $("input[name='username']").val();
+        var password = $("input[name='password']").val();
+
+        if(!username){
+            dialog.error('用户名不能为空!');
+        }
+
+        if(!password){
+            dialog.error('密码不能为空!');
+        }
+
+        var url = "/admin/login/check";
+        var data = {'username':username,'password':password};
+        //执行异步请求
+        $.post(url,data,function(result){
+            if(result.status == 0){
+                dialog.error(result.message);
+            }
+        },'JSON');
+    }
+}
